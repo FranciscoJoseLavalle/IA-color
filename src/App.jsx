6e9308f9ea2fab4e1f77576dbcfb61c55946562c
Francisco <franlavalle@hotmail.com>
2023-06-14 21:10:16 -0300

    Optimizo la carga

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,16 @@ import { db } from '../config/firebase'
 import { collection, getDocs, addDoc } from 'firebase/firestore'
 
 function App() {
-  const guessColor = new brain.NeuralNetwork();
+  // const guessColor = new brain.NeuralNetwork({hiddenLayers: [1]}); // 16.5 segundos
+  const guessColor = new brain.NeuralNetwork({ hiddenLayers: [3] }); // 20, 21
+  // const guessColor = new brain.NeuralNetwork({hiddenLayers: [2]}); // 19 segundos
   const [random, setRandom] = useState([]);
   const [colors, setColors] = useState([]);
   const [color, setColor] = useState('#000000');
+
+  const [r, setR] = useState(0);
+  const [g, setG] = useState(0);
+  const [b, setB] = useState(0);
 
   const colorsCollectionRef = collection(db, "colors")
 
@@ -23,7 +29,29 @@ function App() {
   }
 
   const findColor = () => {
-    guessColor.train(colors)
+    guessColor.train(colors, {
+      iterations: 2500
+    })
+    console.log({ colors });
+
+    // guessColor.trainAsync(colors, {
+    //   iterations: 2500
+    // })
+    //   .then(res => {
+    //     console.log(res);
+    //     let r = parseInt(color.substr(1, 2), 16)
+    //     let g = parseInt(color.substr(3, 2), 16)
+    //     let b = parseInt(color.substr(5, 2), 16)
+    //     console.log(r, g, b);
+    //     let salida = guessColor.run([r / 255, g / 255, b / 255])
+    //     let array = Object.entries(salida);
+    //     array.sort((a, b) => b[1] - a[1])
+    //     document.querySelector(".result").textContent = array[0][0]
+    //     console.log(array);
+    //   })
+    //   .catch(err => console.log(err))
+
+
     let r = parseInt(color.substr(1, 2), 16)
     let g = parseInt(color.substr(3, 2), 16)
     let b = parseInt(color.substr(5, 2), 16)
@@ -44,7 +72,9 @@ function App() {
     console.log(colors);
     getRandomColor();
   }
-
+  const reverseColor = () => {
+    console.log({ r: r * 255, g: g * 255, b: b * 255 })
+  }
   const getRandomColor = () => {
     let r = parseInt((Math.random() * 255).toFixed(0))
     let g = parseInt((Math.random() * 255).toFixed(0))
@@ -68,6 +98,15 @@ function App() {
         <input type="text" className="colorName" placeholder="Nombre del color" required />
         <button className="train" onClick={train}>Entrenar</button>
       </div>
+      {/* <div>
+        <input type="text" onChange={(e) => setR(e.target.value * 255)} placeholder='Red' />
+        <input type="text" onChange={(e) => setG(e.target.value * 255)} placeholder='Green' />
+        <input type="text" onChange={(e) => setB(e.target.value * 255)} placeholder='Blue' />
+        <button onClick={reverseColor}>Reverse</button>
+        <div style={{
+          backgroundColor: `rgb(${r},${g},${b})`
+        }}>color</div>
+      </div> */}
     </main>
   )
 }
